dd2ac16bbc9315220d713ba1986c4cb85bd11961
dlolh <dlolhd@gmail.com>
2016-05-06 19:14:03 +0200

    Añadidos Items nuevo Aviso y salir al menú de Recordatorios

--- app/src/main/java/es/diego3l/recordatorios/RecordatorioActivity.java
+++ app/src/main/java/es/diego3l/recordatorios/RecordatorioActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -45,16 +46,17 @@ public class RecordatorioActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.accion_nuevo:
+                //crear nuevo aviso
+                Log.d(getLocalClassName(), "crear  nuevo Aviso");
+                return true;
+            case R.id.accion_salir:
+                //Finalizar o salir
+                finish();
+                return true;
+            default:
+                return false;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
